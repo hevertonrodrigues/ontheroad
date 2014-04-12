6da2a2da4bd490840ba9526974cf8da36e0114b4
Heverton Rodrigues <heverton25@gmail.com>
2014-04-12 19:14:16 -0300

    Initial Commit

---
+++ OnTheRoad/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OnTheRoad
+//
+//  Created by Heverton Rodrigues on 4/12/14.
+//  Copyright (c) 2014 hackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OnTheRoad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OnTheRoad
+//
+//  Created by Heverton Rodrigues on 4/12/14.
+//  Copyright (c) 2014 hackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
